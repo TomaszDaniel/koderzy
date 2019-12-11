495cc881519446c085e3cf54993ef5a4d1aa5352
Tomasz Daniel <tomaszz.daniel@gmail.com>
2019-12-11 01:29:19 +0100

    fetch questions and answers

--- public/script.js
+++ public/script.js
@@ -1,12 +1,33 @@
 const question = document.querySelector('#question')
-const answer1 = document.querySelector('#answer1')
-const answer2 = document.querySelector('#answer2')
-const answer3 = document.querySelector('#answer3')
-const answer4 = document.querySelector('#answer4')
+const gameBoard = document.querySelector('#game-board')
+const h2 = document.querySelector('h2')
 
 function fillQuestionElements(data) {
+
+    if (data.winner === true) {
+        gameBoard.style.display = 'none';
+        h2.innerText = "WYGRAŁEŚ/AŚ";
+        return;
+    }
+
+    if (data.loser === true) {
+        gameBoard.style.display = 'none';
+        h2.innerText = "Nie poszło tym razem, spróbuj ponownie";
+        return;
+    }
+
+
     question.innerText = data.question;
-    
+
+    // for (const i in data.answers) {
+    //     const answerEl = document.querySelector(`#answer${Number(i) + 1}`);
+    //     answerEl.innerText = data.answers[i]
+    // }
+
+    data.answers.forEach((element, i) => {
+        const answerEl = document.querySelector(`#answer${Number(i) + 1}`);
+        answerEl.innerText = element;
+    })
 }
 
 function showNextQuestion() {
@@ -20,3 +41,29 @@ function showNextQuestion() {
 }
 
 showNextQuestion();
+
+const goodAnswersSpan = document.querySelector('#good-answers')
+
+function handeAnswerFeedback(data) {
+    goodAnswersSpan.innerText = data.goodAnswers;
+    showNextQuestion();
+}
+
+function sendAnswer(answerIndex) {
+    fetch(`/answer/${answerIndex}`, {
+        method: 'POST',
+    })
+        .then(r => r.json())
+        .then(data => {
+            handeAnswerFeedback(data);
+        })
+}
+
+const buttons = document.querySelectorAll('button')
+
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+        const answerIndex = event.target.dataset.answer
+        sendAnswer(answerIndex)
+    })
+}
